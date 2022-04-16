1f669b4ac6b18f5228ad0dad248ad8e4a7081322
Hyeonho-Lee <prohenho7138@gmail.com>
2022-04-16 13:16:42 +0900

    update files

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'material test app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MaterialFlutterApp(),
+      home:HttpApp(),
     );
   }
 }
 
-class MaterialFlutterApp extends StatefulWidget {
+class HttpApp extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() {
-    return _MaterialFlutterApp();
-  }
+  State createState() => _HttpApp();
 }
 
-class _MaterialFlutterApp extends State<MaterialFlutterApp> {
+class _HttpApp extends State {
+  int page = 1;
+  var response;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Material Design App')
-      ),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add_box),
-        onPressed: () {
-
-        }
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset('image/flutter_image.jpg'),
-              Text('Flutter Image View')
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          )
+        appBar: AppBar(
+          title: Text('Http Example'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  getJSONDate();
+                },
+                icon: Icon(Icons.search))
+          ],
         )
-      )
     );
+  }
+
+  Future getJSONDate() async{
+    var url = 'http://3.15.67.95:5050/';
+
+    if (response == null) {
+      response = await http.get(Uri.parse(url));
+    }
+
+    setState(() {
+      if (response.statusCode == 200) {
+        String text_replace = response.body;
+        text_replace = text_replace.replaceAll('&#39;', '"');
+
+        var json_decode = jsonDecode(text_replace);
+        print(json_decode["행사명"]);
+        print(json_decode["개최장소"]);
+        print(json_decode["위도"]);
+        print(json_decode["경도"][1.toString()]);
+      }
+    });
   }
 }
